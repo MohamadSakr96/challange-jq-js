6863cfa56cb931e4424629c54277188c6bdcca40
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-01-27 17:55:49 +0200

    add: js solution

--- js/jquery_solution.js
+++ js/jquery_solution.js
@@ -1,15 +1,20 @@
 // Setup
 var level = [];
 var color_class = [".green", ".red", ".yellow", ".blue"];
-var game_state = true;
 var pressed_btn_number = 0;
-var round_state = true;
 
-if (game_state == true) {
+
+$(document).ready(() => {
+    startGame();
+});
+    
+function startGame(){
     $("body").click(startRound);
-    game_state = false;
+    $("body").keydown(startRound);
+    $("body").unbind("click",startGame);
+    $("body").unbind("keydown",startGame);
 }
-
+    
 function startRound() {
     pressed_btn_number = 0;
     removeEvent();
@@ -18,10 +23,10 @@ function startRound() {
 
 function nextLevel() {
     let n = Math.floor(Math.random()*4);
-    level.push(color_class[n]);
+    level.push(color_class[n]); // num
     $("#title").text(`Level ${level.length}`);
     $(`${level[level.length-1]}`).fadeOut(150);
-    // ------------------------------------------------------- Sound effect
+    // ---------------------------------------------------------- add sound
     $(`${level[level.length-1]}`).fadeIn(150);
     console.log(level);
     playerTurn();
@@ -41,10 +46,11 @@ function checkBtn(s) {
     console.log(s,pressed_btn_number)
     if (s != level[pressed_btn_number]) {
         gameOver();
-    }
-    pressed_btn_number += 1;
-    if (pressed_btn_number >= level.length) {
-        startRound();
+    }else {
+        pressed_btn_number += 1;
+        if (pressed_btn_number >= level.length) {
+            startRound();
+        }
     }
 }
 
@@ -58,14 +64,21 @@ function btnFlash(btn_class) {
 }
 
 function gameOver() {
-    game_state = false;
-    console.log("You Lost");
-
+    $("#title").text(`Game Over, Press Any Key to Restart`);
+    $("body").addClass("game-over");
+    // ------------------------------------------------------------------------- add sound
+    setTimeout(() => {
+        $("body").removeClass("game-over");
+    }, 150);
+    level = [];
     removeEvent();
+    $("body").click(startGame);
+    $("body").keydown(startGame);
 }
 
 function removeEvent() {
     $("body").unbind('click', startRound);
+    $("body").unbind('keydown', startRound);
     $(".btn").unbind('click', btnFlash);
     for (let i=0; i<color_class.length; i++) {
         $(`${color_class[i]}`).unbind();
---
+++ js/js_solution.js
@@ -0,0 +1,105 @@
+// Setup
+var level = [];
+var color_class = ["green", "red", "yellow", "blue"];
+var pressed_btn_number = 0;
+
+var all_btns = document.getElementsByClassName("btn");
+
+window.onload = () => {
+    startGame();
+}
+    
+function startGame(){
+    document.getElementsByTagName("body")[0].addEventListener('click', startRound); 
+    document.getElementsByTagName("body")[0].addEventListener('keydown', startRound); 
+    document.getElementsByTagName("body")[0].removeEventListener('click', startGame);
+    document.getElementsByTagName("body")[0].removeEventListener('keydown', startGame);
+}
+    
+function startRound() {
+    pressed_btn_number = 0;
+    removeEvent();
+    setTimeout(nextLevel, 300);
+}
+
+function nextLevel() {
+    let n = Math.floor(Math.random()*4);
+    level.push(color_class[n]);
+    document.getElementById("title").innerText = `Level ${level.length}`;
+    // ---------------------------------------------------------- add sound
+    setTimeout(() => {
+        document.getElementById(`${color_class[n]}`).style.opacity = 0.5;
+    }, 50);
+    setTimeout(() => {
+        document.getElementById(`${color_class[n]}`).style.opacity = 0;
+    }, 100);
+    setTimeout(() => {
+        document.getElementById(`${color_class[n]}`).style.opacity = 0.5;
+    }, 150);
+    setTimeout(() => {
+        document.getElementById(`${color_class[n]}`).style.opacity = 1;
+    }, 200);
+    playerTurn();
+}
+
+function playerTurn() {
+    let pressed_btn = "";
+    for (let i=0; i<color_class.length; i++) {
+        document.getElementById(`${color_class[i]}`).addEventListener("click", () => {
+            pressed_btn = btnFlash(color_class[i]);
+            console.log(pressed_btn, pressed_btn_number)
+            checkBtn(pressed_btn);
+        });
+    }
+}
+
+function checkBtn(s) {
+    console.log("yo ",s,pressed_btn_number);
+    if (s != level[pressed_btn_number]) {
+        gameOver();
+    }else {
+        pressed_btn_number += 1;
+        if (pressed_btn_number >= level.length) {
+            startRound();
+        }
+    }
+}
+
+function btnFlash(btn_class) {
+    // ------------------------------------------------------------------------- add sound
+    document.getElementById(`${btn_class}`).classList.add("pressed");
+    setTimeout(() => {
+        document.getElementById(`${btn_class}`).classList.remove("pressed");
+    }, 150);
+    return btn_class;
+}
+
+function gameOver() {
+    document.getElementById("title").innerText = `Game Over, Press Any Key to Restart`;
+
+    // ------------------------------------------------------------------------- add sound
+    
+    document.getElementsByTagName("body")[0].classList.add("game-over");
+    setTimeout(() => {
+        document.getElementsByTagName("body")[0].classList.remove("game-over");
+    }, 150);
+
+    level = [];
+    removeEvent();
+
+    document.getElementsByTagName("body")[0].addEventListener('click', startGame); 
+    document.getElementsByTagName("body")[0].addEventListener('keydown', startGame); 
+}
+
+function removeEvent() {
+    document.getElementsByTagName("body")[0].removeEventListener('click', startRound); 
+    document.getElementsByTagName("body")[0].removeEventListener('keydown', startRound); 
+    
+    for (let i=0; i<all_btns.length; i++) {
+        all_btns[i].removeEventListener('click', btnFlash);
+    }
+
+    for (let i=0; i<color_class.length; i++) {
+        document.getElementById(`${color_class[i]}`).removeEventListener('click', ()=>{});
+    }
+}
